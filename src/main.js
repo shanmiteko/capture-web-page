bd7a370eebd81e5b0ac8d758365821363c5e545a
shanmite <1971513084@qq.com>
2020-12-18 22:31:46 +0800

    fix: json ""

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const { IfNotExistCreateFile } = require("./lib/utils");
 (function () {
     Promise.resolve()
         .then(() => process.env.BILICOOKIES || '')
-        .then(BILICOOKIES => IfNotExistCreateFile('src/config/secret.json', `{"BiliCOOKIE": ${BILICOOKIES}}`))
+        .then(BILICOOKIES => IfNotExistCreateFile('src/config/secret.json', `{"BiliCOOKIE": "${BILICOOKIES}}"`))
         .then(() => screenshot('https://www.bilibili.com'))
         .then(filename => require("./lib/upload").uploadToBili(filename))
         .then(res => {
