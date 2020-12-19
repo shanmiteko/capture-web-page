d1bd3d517c2f2e367deef3ddc40b429bb0a6fbbb
shanmite <1971513084@qq.com>
2020-12-19 12:28:18 +0800

    updata: wiki Rust

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const { screenshot } = require("./lib/screenshot");
-const { IfNotExistCreateFile } = require("./lib/utils");
+const { screenshot } = require("./src/lib/screenshot");
+const { IfNotExistCreateFile } = require("./src/lib/utils");
 
 (function () {
-    const link = 'https://zh.wikipedia.org/wiki/Javascript';
+    const link = 'https://zh.wikipedia.org/wiki/Rust';
     // const link = 'https://www.bilibili.com/';
     Promise.resolve()
         .then(() => process.env.BILI || '')
         .then(BILI => IfNotExistCreateFile('src/config/secret.json', JSON.stringify({ BiliCOOKIE: BILI })))
         .then(() => screenshot(link))
-        .then(filename => require("./lib/upload").uploadToBili(filename))
+        .then(filename => require("./src/lib/upload").uploadToBili(filename))
         .then(res => {
             if (res.code === 0) return res.data.image_url
             return Promise.reject('message: ' + res.message)
